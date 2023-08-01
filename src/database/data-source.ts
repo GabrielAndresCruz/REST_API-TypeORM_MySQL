ed965f6eb408d3069bd600460a6302f07232d37e
GabrielAndresCruz <andrescruz048@hotmail.com>
2023-07-31 22:55:02 -0300

    Database connected. Adding MySQL container

---
+++ src/database/data-source.ts
@@ -0,0 +1,17 @@
+import * as dotenv from "dotenv";
+import { DataSource } from "typeorm";
+
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: "mysql",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  logging: true,
+  synchronize: false, //synchronize - Indicates if database schema should be auto created on every application launch. Be careful with this option and don't use this in production - otherwise you can lose production data. This option is useful during debug and development. As an alternative to it, you can use CLI and run schema:sync command.
+  entities: [],
+  subscribers: [],
+});
