58aac179a18a198d125650601cbb3131c493f33a
GabrielAndresCruz <andrescruz048@hotmail.com>
2023-08-01 21:34:25 -0300

    Global reusable Response

--- src/controllers/AuthorsController.ts
+++ src/controllers/AuthorsController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { AppDataSource } from "../database/data-source";
 import { Author } from "../entities/Author";
+import { ResponseUtl } from "../../utils/Response";
 
 export class AuthorsController {
   async getAuthors(req: Request, res: Response) {
@@ -27,11 +28,11 @@ export class AuthorsController {
       const author = await AppDataSource.getRepository(Author).findOneByOrFail({
         id: Number(id),
       });
-      return res.status(200).json({
-        success: true,
-        message: "Fetched author successfully",
-        data: author,
-      });
+      return ResponseUtl.sendResponse<Author>(
+        res,
+        "Fetch author successfully",
+        author
+      );
     } catch (error) {
       console.error("Error fetching author:", error);
       return res.status(500).json({
